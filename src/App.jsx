88ccb2b21feb889cd5dd2c234f97cff84f97d6ab
Ornella Facciola <ornefacciola@gmail.com>
2024-04-18 19:36:14 -0300

    Commiting navbar and images

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/Footer'
 import Card from './components/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
-
-     <Footer />
-     <Card
+    <Navbar/>
+    <Footer />
+    <Card
       imgSrc="https://foter.com/photos/400/alfonsina-free-standing-swirl-toilet-paper-holder.jpg"
       imgAlt="Card Image 1"
       title="Product"
       description="Description"
       price="$299"
-      />  
-      
+    />  
     </>
 
   
