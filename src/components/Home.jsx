549847b0f488318601cac4fe19a60a9dbfe7e922
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-04-19 14:11:42 -0300

    chore: home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Footer from './Footer';
 import CategoriesBar from './CategoriesBar';
+import '../App.css';
 
 const Home = () => {
     return (
         <>
         
-            <CategoriesBar />
-            
+                <CategoriesBar />
+                <div className='home-container'>
+                <div class="image-container">
+                <img src="https://www.gardenandhome.co.za/wp-content/uploads/2023/02/alexandra-gorn-JIUjvqe2ZHg-unsplash.jpg"></img>
+                <h1>Viví la experiencia de disfrutar un hogar hecho especialmente para vos</h1>
+                </div>
+                </div>
         </>
     );
 };
