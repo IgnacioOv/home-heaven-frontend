251e22be034b5176be4dcbb6ad32982b87ff3287
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-04-19 14:16:44 -0300

    updated app.jsx, missing to resolve merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
 import './App.css'
 import Footer from './components/Footer'
 import Card from './components/Card';
-<<<<<<< HEAD
-import Home from './components/Home'
-=======
 import Navbar from './components/Navbar/Navbar';
->>>>>>> 847e9af4553e07b72113a49d3684028c248fb5fb
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
-
-     <Home />
-     <Card
-=======
     <Navbar/>
     <Card
->>>>>>> 847e9af4553e07b72113a49d3684028c248fb5fb
       imgSrc="https://foter.com/photos/400/alfonsina-free-standing-swirl-toilet-paper-holder.jpg"
       imgAlt="Card Image 1"
       title="Product"
       description="Description"
       price="$299"
-<<<<<<< HEAD
-      />  
-      <Footer />
-=======
     />  
     <Footer />
->>>>>>> 847e9af4553e07b72113a49d3684028c248fb5fb
     </>
 
   
