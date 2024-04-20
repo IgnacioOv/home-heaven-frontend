76845cdab8caf595ebb503ba3aa8c380bf54ee63
Ornella Facciola <ornefacciola@gmail.com>
2024-04-19 22:14:20 -0300

    Cambie el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,31 @@
 import logoImage from '../../images/homeHLogo.png';
 import gitFooter from '../../images/gf.png';
 import homeFooter from '../../images/hf.png';
+import './Footer.css';
 
 const Footer = () => {
 return (
 <footer className="footer">
     <div className="footer-line"></div>
-    <div className="footer-content">
+    <div className="footer-top">
     <div className="footer-column">
-        {/* Logo de la empresa */}
-        <img src={logoImage} alt="Logo" style={{ width: '35%', height: '35%' }} />
+        {/* Company logo */}
+        <img src={logoImage} alt="Logo" className="footer-logo" />
     </div>
     <div className="footer-column footer-middle">
-        <h3 style={{ fontSize:'1em' }} >Calidad.Experiencia.Excelencia.</h3>
-        <p  style={{ fontSize:'0.5em'}}> Son nuestro sello distintivo. Déjanos transformar tu casa en un espacio único y acogedor.</p>
+        <h3>Calidad.Experiencia.Excelencia.</h3>
+        <p>Son nuestro sello distintivo. Déjanos transformar tu casa en un espacio único y acogedor.</p>
     </div>
     <div className="footer-column">
-        {/* Logos pequeños */}
-        <img src={homeFooter} alt="Profile" style={{ width: '12%', height: '12%', cursor:'pointer'}} />
-        <img src={gitFooter} alt="Cart" style={{ width: '12%', height: '12%', marginLeft:'5%', cursor:'pointer'}} />
+        {/* Small logos */}
+        <img src={homeFooter} alt="Profile" className="footer-small-logo" />
+        <img src={gitFooter} alt="Cart" className="footer-small-logo" />
     </div>
     </div>
     <div className="footer-line"></div>
     <div className="footer-bottom">
-    {/* Texto de derechos de autor y logo */}
-    <p  style={{ fontSize:'0.7em'}}> © All rights reserved</p>
+    {/* Copyright text and logo */}
+    <p>© All rights reserved</p>
     </div>
 </footer>
 );
