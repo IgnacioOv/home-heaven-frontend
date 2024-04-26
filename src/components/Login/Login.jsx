a668449beb64588e36eb327117c659e34a34fa48
lugaliano <luciaagaliano@gmail.com>
2024-04-25 21:03:28 -0300

    adding register component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom'; // Importa Link desde react-router-dom
+
 import './Login.css';
 import logoImage from '../../images/homeHLogo.png';
 
@@ -8,6 +11,12 @@ const Login = () => {
             <div className='login-container'>
                 
                 <div className='login-card'>
+                <Link to="/" className="goBack" style={{color:'rgb(117,127,126,1)'}} >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512" className="icon-submenu" fill="currentColor" >
+                        <path d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path>
+                    </svg>
+                    Inicio
+                </Link>
                 <div className="logo">
                 <img src={logoImage} alt="Logo" className='logoH' />
                 <h3>Inicia sesión</h3>
@@ -31,7 +40,7 @@ const Login = () => {
 
 
                     
-                    <p>¿Recién nos conoces? <b>REGISTRATE</b></p>
+                <p>¿Recién nos conoces? <Link to="/register"style={{color:'rgb(117,127,126,1)'}}><b>REGISTRATE</b></Link></p>
 
                 </div>
                 
