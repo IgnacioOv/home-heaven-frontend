dd998249684687f9de36af593566be346462ac1b
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-04-26 21:09:31 -0300

    adds local db with json server and product page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import './Card.css';
+import { Link } from 'react-router-dom'
 
 
-export const Card = ({
-    imgSrc,
-    imgAlt,
-    title,
-    description,
-    price,
+export const Card = ({product}) => {
 
-  }) => {
+  
     return (
       <div className="card-container">
-        <img src={imgSrc} alt={imgAlt} className="card-img" />
-        <h2 className="card-title">{title}</h2>
-        <p className="card-description">{description}</p>
-        <p className="card-price">{"$" + price}</p>
+        <img src={product.image} alt={product.description} className="card-img" />
+        <p className="card-title">{product.product}</p>
+        <Link to={`/product/${product.id}`} className="ver-mas">Ver mas</Link>
+        <p className="card-price">{"$" + product.price}</p>
       </div>
     );
   };
