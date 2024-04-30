cc1a8ebd62d3ec821886737156cbbb1550e5f8eb
Nicolas <nhan@uade.edu.ar>
2024-04-30 20:05:17 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,7 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register'; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductPage from './components/ProductPage/ProductPage';
-<<<<<<< HEAD
-import Filter from './components/Filter/Filter';
-import CategoryPage from './components/CategoryPage/CategoryPage';
 
-=======
->>>>>>> 3304c1ae27201ad1f777b54cf1e293b25064e6c8
 function App() {
   return (
     <BrowserRouter>
@@ -20,13 +15,7 @@ function App() {
         <Route path="/" element={<> <Navbar /> <Home /> <Footer /> </>} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-<<<<<<< HEAD
-        <Route path="product/:id" element={<><Navbar/> <ProductPage/><Footer /></>}/>
-        <Route path="products" element={<><Navbar/><Filter/><Footer /></>}/>
-        <Route path="/category/:categoryName" element={<CategoryPage />} />
-=======
         <Route path="product/:id" element={<><Navbar/><ProductPage/><Footer /></>}/>
->>>>>>> 3304c1ae27201ad1f777b54cf1e293b25064e6c8
       </Routes>
     </BrowserRouter>
   );
