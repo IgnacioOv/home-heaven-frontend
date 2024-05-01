3445b63f60d0662c854ec3be62bc5d266bd11a4e
Ignacio Oliva Velez <nachoolivavelez18@gmail.com>
2024-05-01 16:38:22 -0300

    fixed wrong merge resolve

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,24 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register'; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductPage from './components/ProductPage/ProductPage';
-//import { CartProvider } from './components/CartProvider'; // import CartProvider
 import Footer from './components/Footer/Footer';
 import { CartProvider } from './context/CartProvider';
 import CategoryPage from './components/CategoryPage/CategoryPage';
-import SearchResult from './components/SearchResult/SearchResult';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<> <Navbar /> <Home /> <Footer /> </>} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="product/:id" element={<><Navbar/><ProductPage/><Footer /></>}/>
-        <Route path="/category/:categoryName" element={<><CategoryPage /> <Footer/> </>} />
-        <Route path="search" element={<><Navbar /> <SearchResult/><Footer /> </>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <CartProvider> 
+        <Routes>
+          <Route path="/" element={<> <Navbar /> <Home /> <Footer /> </>} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="product/:id" element={<><Navbar/><ProductPage/><Footer /></>}/>
+          <Route path="/category/:categoryName" element={<><CategoryPage /> <Footer/> </>} />
+        </Routes>
+        </CartProvider> 
+      </BrowserRouter>
+
   );
 }
 
