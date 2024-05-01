cd7ff8fbdc28e21562e28b8720d9f9a0e00c472d
Nicolas <nhan@uade.edu.ar>
2024-04-30 21:44:51 -0300

    final fix

--- src/components/CategoriesBar/CategoriesBar.jsx
+++ src/components/CategoriesBar/CategoriesBar.jsx
@@ -1,16 +1,17 @@
 import '../../App.css';
 import './CategoriesBar.css';
+import { Link } from 'react-router-dom';
 
 const CategoriesBar = () => {
     return (
         <>
         <ul className='categories-bar'>
-            <li className='category'>TODOS</li>
-            <li className='category'>BAÑO</li>
-            <li className='category'>COCINA</li>
-            <li className='category'>DECORACIÓN</li>
-            <li className='category'>DORMITORIO</li>
-            <li className='category'>LIVING</li>
+        <li className='category'><Link to="/category/TODOS">TODOS</Link></li>
+            <li className='category'><Link to="/category/BAÑO">BAÑO</Link></li>
+            <li className='category'><Link to="/category/COCINA">COCINA</Link></li>
+            <li className='category'><Link to="/category/DECORACIÓN">DECORACIÓN</Link></li>
+            <li className='category'><Link to="/category/DORMITORIO">DORMITORIO</Link></li>
+            <li className='category'><Link to="/category/LIVING">LIVING</Link></li>
         </ul> 
         </>
     );
